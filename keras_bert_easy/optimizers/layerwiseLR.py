cace573accae10a862e52ca77f1db395c834f0d7
washington <947268074@qq.com>
2020-08-10 14:44:43 +0800

    将原来分开的keras/tf.keras接口整合一起，通过环境变量`TF_KERAS`来区分和调用对应的optimizer接口

--- keras_bert_easy/optimizers/layerwiseLR.py
+++ keras_bert_easy/optimizers/layerwiseLR.py
@@ -2,10 +2,19 @@
 # 用 BERT / XLNET / ALBERT / RoBERTa / ELECTRA...做fine-tune，
 # 使用分层学习率的优化器，往往能得到更好的效果。
 
+import os
+from distutils.util import strtobool
 import tensorflow as tf
-from tensorflow.python.keras.optimizers import Optimizer as tfkerasOptimizer
-import keras
-import keras.backend as K
+
+TF_KERAS = strtobool(os.environ.get('TF_KERAS', '0'))
+if TF_KERAS:
+    keras = tf.keras
+    from tensorflow.python.keras.optimizers import Optimizer as tfkerasOptimizer
+    BaseOptimizer = tfkerasOptimizer
+else:
+    import keras
+    BaseOptimizer = keras.optimizers.Optimizer
+K = keras.backend
 
 
 def get_weights_by_layernames(model, layernames, use_tfkeras=False):
@@ -23,9 +32,18 @@ def get_weights_by_layernames(model, layernames, use_tfkeras=False):
     return weights
 
 
-class keras_Adam_2lr(keras.optimizers.Optimizer):
+def keras_mode_hint():
+    if TF_KERAS:
+        print('using tf.keras API. tf version: %s' % tf.__version__)
+    else:
+        version = keras.__version__
+        if version > '2.3.1':
+            raise ImportError('keras version should not be greater than 2.3.1, got %s' % version)
+        print('using keras API. keras version: %s' % version)
+
+
+class keras_Adam_2lr(BaseOptimizer):
     """支持2段式学习率的Adam optimizer.
-        针对keras编写（切勿使用tf.keras），且要求keras.__version__ <= 2.3.1
     # Arguments
         final_layers: list of final layers' weights (layers close to output layer, including output layer)
         lr: float >= 0. List of Learning rates. [Early layers, Final Layers]
@@ -42,6 +60,9 @@ class keras_Adam_2lr(keras.optimizers.Optimizer):
                  beta_1=0.9, beta_2=0.999,
                  epsilon=None, decay=0., amsgrad=False, **kwargs):
         super(keras_Adam_2lr, self).__init__(**kwargs)
+
+        keras_mode_hint()
+
         with K.name_scope(self.__class__.__name__):
             self.iterations = K.variable(0, dtype='int64', name='iterations')
             self.lrate = K.variable(lr, name='lr')
@@ -122,108 +143,8 @@ class keras_Adam_2lr(keras.optimizers.Optimizer):
         return get_weights_by_layernames(model, layernames, use_tfkeras=False)
 
 
-class tfkeras_Adam_2lr(tfkerasOptimizer):
-    """支持2段式学习率的Adam optimizer.
-        针对tf.keras编写（切勿使用keras）
-    # Arguments
-        final_layers: list of final layers' weights (layers close to output layer, including output layer)
-        lr: float >= 0. List of Learning rates. [Early layers, Final Layers]
-        beta_1: float, 0 < beta < 1. Generally close to 1.
-        beta_2: float, 0 < beta < 1. Generally close to 1.
-        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
-        decay: float >= 0. Learning rate decay over each update.
-        amsgrad: boolean. Whether to apply the AMSGrad variant of this
-            algorithm from the paper "On the Convergence of Adam and
-            Beyond".
-    """
-
-    def __init__(self, final_layers, lr=(1e-5, 1e-4),
-                 beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., amsgrad=False, **kwargs):
-        super(tfkeras_Adam_2lr, self).__init__(**kwargs)
-        with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            self.lrate = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-
-            self.final_layers = final_layers
-
-        if epsilon is None:
-            epsilon = K.epsilon()
-        self.epsilon = epsilon
-        self.initial_decay = decay
-        self.amsgrad = amsgrad
-
-    def get_updates(self, loss, params):
-        grads = self.get_gradients(loss, params)
-        self.updates = [K.update_add(self.iterations, 1)]
-
-        lr = self.lrate
-        if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
-                                                      K.dtype(self.decay))))
-
-        t = K.cast(self.iterations, K.floatx()) + 1
-        lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
-                     (1. - K.pow(self.beta_1, t)))
-
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        if self.amsgrad:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        else:
-            vhats = [K.zeros(1) for _ in params]
-        self.weights = [self.iterations] + ms + vs + vhats
-
-        # Setting lr of the initial layers
-        lr_grp = lr_t[0]
-        for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-
-            # Updating lr when the split layer is encountered
-            if p.name in self.final_layers:
-                lr_grp = lr_t[1]
-
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-            if self.amsgrad:
-                vhat_t = K.maximum(vhat, v_t)
-                p_t = p - lr_grp * m_t / (K.sqrt(vhat_t) + self.epsilon)  # Using updated lr
-                self.updates.append(K.update(vhat, vhat_t))
-            else:
-                p_t = p - lr_grp * m_t / (K.sqrt(v_t) + self.epsilon)
-
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
-            new_p = p_t
-
-            # Apply constraints.
-            if getattr(p, 'constraint', None) is not None:
-                new_p = p.constraint(new_p)
-
-            self.updates.append(K.update(p, new_p))
-        return self.updates
-
-    def get_config(self):
-        config = {
-            'lr': (K.get_value(self.lrate)),
-            'beta_1': float(K.get_value(self.beta_1)),
-            'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
-            'epsilon': self.epsilon,
-            'amsgrad': self.amsgrad}
-        base_config = super(tfkeras_Adam_2lr, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-    @staticmethod
-    def get_weights_by_layernames(model, layernames):
-        return get_weights_by_layernames(model, layernames, use_tfkeras=True)
-
-
-class keras_Adam_3lr(keras.optimizers.Optimizer):
+class keras_Adam_3lr(BaseOptimizer):
     """支持3段式学习率的Adam optimizer.
-        针对keras编写（切勿使用tf.keras），且要求keras.__version__ <= 2.3.1
     # Arguments
         middle_layers: list of middle layers' weights
         final_layers: list of final layers' weights (layers close to output layer, including output layer)
@@ -241,6 +162,9 @@ class keras_Adam_3lr(keras.optimizers.Optimizer):
                  beta_1=0.9, beta_2=0.999,
                  epsilon=None, decay=0., amsgrad=False, **kwargs):
         super(keras_Adam_3lr, self).__init__(**kwargs)
+
+        keras_mode_hint()
+
         with K.name_scope(self.__class__.__name__):
             self.iterations = K.variable(0, dtype='int64', name='iterations')
             self.lrate = K.variable(lr, name='lr')
@@ -325,115 +249,11 @@ class keras_Adam_3lr(keras.optimizers.Optimizer):
         return get_weights_by_layernames(model, layernames, use_tfkeras=False)
 
 
-class tfkeras_Adam_3lr(tfkerasOptimizer):
-    """支持3段式学习率的Adam optimizer.
-        针对tf.keras编写（切勿使用keras）
-    # Arguments
-        middle_layers: list of middle layers' weights
-        final_layers: list of final layers' weights (layers close to output layer, including output layer)
-        lr: float >= 0. List of Learning rates. [Early layers, Middle layers, Final Layers]
-        beta_1: float, 0 < beta < 1. Generally close to 1.
-        beta_2: float, 0 < beta < 1. Generally close to 1.
-        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
-        decay: float >= 0. Learning rate decay over each update.
-        amsgrad: boolean. Whether to apply the AMSGrad variant of this
-            algorithm from the paper "On the Convergence of Adam and
-            Beyond".
-    """
-
-    def __init__(self, middle_layers, final_layers, lr=(1e-5, 1e-4, 1e-2),
-                 beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., amsgrad=False, **kwargs):
-        super(tfkeras_Adam_3lr, self).__init__(**kwargs)
-        with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            self.lrate = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-
-            self.middle_layers = middle_layers
-            self.final_layers = final_layers
-            # print('middle_layers weights:', middle_layers)
-            # print('final_layers weights:', final_layers)
-        if epsilon is None:
-            epsilon = K.epsilon()
-        self.epsilon = epsilon
-        self.initial_decay = decay
-        self.amsgrad = amsgrad
-
-    def get_updates(self, loss, params):
-        grads = self.get_gradients(loss, params)
-        self.updates = [K.update_add(self.iterations, 1)]
-
-        lr = self.lrate
-        if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
-                                                      K.dtype(self.decay))))
-
-        t = K.cast(self.iterations, K.floatx()) + 1
-        lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
-                     (1. - K.pow(self.beta_1, t)))
-
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        if self.amsgrad:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        else:
-            vhats = [K.zeros(1) for _ in params]
-        self.weights = [self.iterations] + ms + vs + vhats
-
-        # Setting lr of the initial layers
-        lr_grp = lr_t[0]
-        for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            # print('params name: ', p.name)
-            # Updating lr when the split layer is encountered
-            if p.name in self.middle_layers:
-                lr_grp = lr_t[1]
-            if p.name in self.final_layers:
-                lr_grp = lr_t[2]
-
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-            if self.amsgrad:
-                vhat_t = K.maximum(vhat, v_t)
-                p_t = p - lr_grp * m_t / (K.sqrt(vhat_t) + self.epsilon)  # Using updated lr
-                self.updates.append(K.update(vhat, vhat_t))
-            else:
-                p_t = p - lr_grp * m_t / (K.sqrt(v_t) + self.epsilon)
-
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
-            new_p = p_t
-
-            # Apply constraints.
-            if getattr(p, 'constraint', None) is not None:
-                new_p = p.constraint(new_p)
-
-            self.updates.append(K.update(p, new_p))
-        return self.updates
-
-    def get_config(self):
-        config = {
-            'lr': (K.get_value(self.lrate)),
-            'beta_1': float(K.get_value(self.beta_1)),
-            'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
-            'epsilon': self.epsilon,
-            'amsgrad': self.amsgrad}
-        base_config = super(tfkeras_Adam_3lr, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-    @staticmethod
-    def get_weights_by_layernames(model, layernames):
-        return get_weights_by_layernames(model, layernames, use_tfkeras=True)
-
-
-class keras_Adam_lr_decay(keras.optimizers.Optimizer):
+class keras_Adam_lr_decay(BaseOptimizer):
     """Adam optimizer that supports layer-wise-decayed learning rate.
        Layer-wise-decayed learning rate means that the learning rate decays
             by the same power from output-layer to input-layer.
-       For use of keras API (don't use tf.keras here)，requiring keras.__version__ <= 2.3.1
+
     # Arguments
         weight_name_to_layer_depth: dict, get the layer depth according to weight name.
             With its layer depth, the weight can be bound with layer-wise-decayed learning rate.
@@ -455,6 +275,9 @@ class keras_Adam_lr_decay(keras.optimizers.Optimizer):
                  lr=1e-3, beta_1=0.9, beta_2=0.999,
                  epsilon=None, decay=0., amsgrad=False, **kwargs):
         super(keras_Adam_lr_decay, self).__init__(**kwargs)
+
+        keras_mode_hint()
+
         with K.name_scope(self.__class__.__name__):
             self.iterations = K.variable(0, dtype='int64', name='iterations')
             self.lrate = K.variable(lr, name='lr')
@@ -538,123 +361,6 @@ class keras_Adam_lr_decay(keras.optimizers.Optimizer):
 
         weight_name_to_layer_depth = {}
         for i, layer in enumerate(keras_model.layers):
-            print('layer name: ', layer.name)
-            depth = i + 1
-            for w in layer.weights:
-                print('\t weights name: ', w.name)
-                weight_name_to_layer_depth[w.name] = depth
-        return weight_name_to_layer_depth
-
-
-class tfkeras_Adam_lr_decay(tfkerasOptimizer):
-    """Adam optimizer that supports layer-wise-decayed learning rate.
-       Layer-wise-decayed learning rate means that the learning rate decays
-            by the same power from output-layer to input-layer.
-       For use of tf.keras API (don't use keras here)
-    # Arguments
-        weight_name_to_layer_depth: dict, get the layer depth according to weight name.
-            With its layer depth, the weight can be bound with layer-wise-decayed learning rate.
-            Output layer gets the biggest depth.
-        n_layers: int, layers of the model (eg. n_layers = len(model.layers))
-        layerwise_lr_decay_power: float, 0 < layerwise_lr_decay_power <= 1.
-            lr of depth m, i.e. lr_m = lr * pow(layerwise_lr_decay_power, (n_layers - m))
-        lr: float >= 0.  Learning rates.
-        beta_1: float, 0 < beta < 1. Generally close to 1.
-        beta_2: float, 0 < beta < 1. Generally close to 1.
-        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
-        decay: float >= 0. Learning rate decay over each update.
-        amsgrad: boolean. Whether to apply the AMSGrad variant of this
-            algorithm from the paper "On the Convergence of Adam and
-            Beyond".
-    """
-
-    def __init__(self, weight_name_to_layer_depth, n_layers, layerwise_lr_decay_power=0.8,
-                 lr=1e-3, beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., amsgrad=False, **kwargs):
-        super(tfkeras_Adam_lr_decay, self).__init__(**kwargs)
-        with K.name_scope(self.__class__.__name__):
-            self.iterations = K.variable(0, dtype='int64', name='iterations')
-            self.lrate = K.variable(lr, name='lr')
-            self.beta_1 = K.variable(beta_1, name='beta_1')
-            self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
-
-        self.weight_name_to_layer_depth = weight_name_to_layer_depth
-        self.n_layers = n_layers
-        self.layerwise_lr_decay_power = layerwise_lr_decay_power
-        if epsilon is None:
-            epsilon = K.epsilon()
-        self.epsilon = epsilon
-        self.initial_decay = decay
-        self.amsgrad = amsgrad
-
-    def get_updates(self, loss, params):
-        grads = self.get_gradients(loss, params)
-        self.updates = [K.update_add(self.iterations, 1)]
-
-        lr = self.lrate
-        if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
-                                                      K.dtype(self.decay))))
-
-        t = K.cast(self.iterations, K.floatx()) + 1
-        lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
-                     (1. - K.pow(self.beta_1, t)))
-
-        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        if self.amsgrad:
-            vhats = [K.zeros(K.int_shape(p), dtype=K.dtype(p)) for p in params]
-        else:
-            vhats = [K.zeros(1) for _ in params]
-        self.weights = [self.iterations] + ms + vs + vhats
-
-        # Setting lr of the initial layers
-        lr_grp = lr_t
-        for p, g, m, v, vhat in zip(params, grads, ms, vs, vhats):
-            # Updating lr according to layer depth (input_layer_depth = 1)
-            depth = self.weight_name_to_layer_depth.get(p.name, self.n_layers)
-            if 1 <= depth <= self.n_layers:
-                lr_grp = lr_t * K.pow(self.layerwise_lr_decay_power, self.n_layers-depth)
-                # print('param name: %s, depth: %d, lr: %s'%(p.name, depth, lr_grp))
-            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
-            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
-            if self.amsgrad:
-                vhat_t = K.maximum(vhat, v_t)
-                p_t = p - lr_grp * m_t / (K.sqrt(vhat_t) + self.epsilon)  # Using updated lr
-                self.updates.append(K.update(vhat, vhat_t))
-            else:
-                p_t = p - lr_grp * m_t / (K.sqrt(v_t) + self.epsilon)
-
-            self.updates.append(K.update(m, m_t))
-            self.updates.append(K.update(v, v_t))
-            new_p = p_t
-
-            # Apply constraints.
-            if getattr(p, 'constraint', None) is not None:
-                new_p = p.constraint(new_p)
-
-            self.updates.append(K.update(p, new_p))
-        return self.updates
-
-    def get_config(self):
-        config = {
-            'lr': (K.get_value(self.lrate)),
-            'beta_1': float(K.get_value(self.beta_1)),
-            'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
-            'epsilon': self.epsilon,
-            'amsgrad': self.amsgrad}
-        base_config = super(tfkeras_Adam_lr_decay, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-    @staticmethod
-    def build_dict_layer_depth(tfkeras_model):
-        if not isinstance(tfkeras_model, tf.keras.Model):
-            raise ValueError("the input `tfkeras_model` must be a instance of tf.keras.Model")
-
-        weight_name_to_layer_depth = {}
-        for i, layer in enumerate(tfkeras_model.layers):
             print('layer name: ', layer.name)
             depth = i + 1
             for w in layer.weights:
--- keras_bert_easy/optimizers/test.py
+++ keras_bert_easy/optimizers/test.py
@@ -1,13 +1,14 @@
+import os
+os.environ['TF_KERAS'] = '1'
 import numpy as np
 import tensorflow as tf
-import keras
 
-import keras.activations
 try:
     from . import layerwiseLR
+    from .layerwiseLR import keras
 except:
     import layerwiseLR
-import keras.backend as K
+    from layerwiseLR import keras
 
 
 def test_keras_Adam_2lr():
@@ -50,48 +51,6 @@ def test_keras_Adam_2lr():
     print('tf version: ', tf.__version__)
     print('keras version: ', keras.__version__)
     print('test_keras_Adam_2lr ok!')
-
-
-def test_tfkeras_Adam_2lr():
-    print('\n--------------------\nbegin to test tfkeras_Adam_2lr...')
-
-    # tf 2.x 需要禁掉Eager
-    if tf.__version__.startswith('2.'):
-        tf.compat.v1.disable_eager_execution()
-
-    # get model
-    inpl = keras.layers.Input(shape=[5,], name='early-layer1')
-    x = keras.layers.Dense(10, name='early-layer2')(inpl)
-    x = keras.layers.Dense(10, name='final-layer1')(x)
-    output = keras.layers.Dense(1, name='final-layer2')(x)
-    model = keras.Model(inpl, output)
-    model.summary()
-
-    # get final_layers_weights
-    final_layers_names = ['final-layer1', 'final-layer2']
-    final_layers_weights= []
-    for name in final_layers_names:
-        final_layers_weights += model.get_layer(name).weights
-    print('final_layers_weights: ', [item.name for item in final_layers_weights])
-
-    # train
-    optimizer = layerwiseLR.keras_Adam_2lr(
-        final_layers=final_layers_weights,
-        lr=(1e-3, 1e-2)  # early_layers, final_layers 学习率分别设为 1e-3, 1e-2
-    )
-    model.compile(
-        optimizer=optimizer,
-        loss=keras.losses.binary_crossentropy
-    )
-    print('model compile done!')
-    x_train = np.random.random(size=[100,5])  # data size: 100
-    y_train = np.array([1, 0]*50)
-    model.fit(x_train, y_train, epochs=3, verbose=2)
-
-    # test success!
-    print('tf version: ', tf.__version__)
-    print('keras version: ', keras.__version__)
-    print('test_tfkeras_Adam_2lr ok!')
 
 
 def test_keras_Adam_3lr():
@@ -143,54 +102,6 @@ def test_keras_Adam_3lr():
     print('test_keras_Adam_3lr ok!')
 
 
-def test_tfkeras_Adam_3lr():
-    print('\n--------------------\nbegin to test tfkeras_Adam_3lr...')
-
-    # tf 2.x 需要禁掉Eager
-    if tf.__version__.startswith('2.'):
-        tf.compat.v1.disable_eager_execution()
-
-    # get model
-    inpl = tf.keras.layers.Input(shape=[5,], name='early-layer1')
-    x = tf.keras.layers.Dense(10, name='early-layer2')(inpl)
-    x = tf.keras.layers.Dense(10, name='middle-layer1')(x)
-    x = tf.keras.layers.Dense(10, name='middle-layer2')(x)
-    x = tf.keras.layers.Dense(10, name='final-layer1')(x)
-    output = tf.keras.layers.Dense(1, name='final-layer2')(x)
-    model = tf.keras.Model(inpl, output)
-    model.summary()
-
-    # get middle_layers_weights, final_layers_weights
-    middle_layers_names = ['middle-layer1', 'middle-layer2']
-    final_layers_names = ['final-layer1', 'final-layer2']
-    middle_layers_weights, final_layers_weights= [], []
-    for name in middle_layers_names:
-        middle_layers_weights += model.get_layer(name).weights
-    for name in final_layers_names:
-        final_layers_weights += model.get_layer(name).weights
-    print('middle_layers_weights: ', [item.name for item in middle_layers_weights])
-    print('final_layers_weights: ', [item.name for item in final_layers_weights])
-
-    # train
-    optimizer = layerwiseLR.tfkeras_Adam_3lr(
-        middle_layers=middle_layers_weights,
-        final_layers=final_layers_weights,
-        lr=(1e-3, 1e-2, 1e-1)  # early_layers, middle_layers, final_layers 学习率分别设为 1e-3, 1e-2, 1e-1
-    )
-    model.compile(
-        optimizer=optimizer,
-        loss=keras.losses.binary_crossentropy
-    )
-    print('model compile done!')
-    x_train = np.random.random(size=[100,5])  # data size: 100
-    y_train = np.array([1, 0]*50)
-    model.fit(x_train, y_train, epochs=3, verbose=2)
-
-    # test success!
-    print('tf version: ', tf.__version__, ', keras version: ', keras.__version__)
-    print('test_tfkeras_Adam_3lr ok!')
-
-
 def test_keras_Adam_lr_decay():
     print('\n--------------------\nbegin to test keras_Adam_lr_decay...')
 
@@ -239,58 +150,8 @@ def test_keras_Adam_lr_decay():
     print('test_keras_Adam_lr_decay ok!')
 
 
-def test_tfkeras_Adam_lr_decay():
-    print('\n--------------------\nbegin to test tfkeras_Adam_lr_decay...')
-
-    # tf 2.x 需要禁掉Eager
-    if tf.__version__.startswith('2.'):
-        tf.compat.v1.disable_eager_execution()
-
-    # get model
-    inpl = tf.keras.layers.Input(shape=[5,], name='layer1')
-    x = tf.keras.layers.Dense(10, name='layer2')(inpl)
-    x = tf.keras.layers.Dense(10, name='layer3')(x)
-    x = tf.keras.layers.Dense(10, name='layer4')(x)
-    x = tf.keras.layers.Dense(10, name='layer5')(x)
-    output = tf.keras.layers.Dense(1, name='layer6')(x)
-    model = tf.keras.Model(inpl, output)
-    model.summary()
-
-    n_layers = len(model.layers)
-    # weight_name_to_layer_depth = {}
-    # for i, layer in enumerate(model.layers):
-    #     print('layer name: ', layer.name)
-    #     depth = i + 1
-    #     for w in layer.weights:
-    #         print('\t weights name: ', w.name)
-    #         weight_name_to_layer_depth[w.name] = depth
-    weight_name_to_layer_depth = layerwiseLR.tfkeras_Adam_lr_decay.build_dict_layer_depth(model)
-
-    # train
-    optimizer = layerwiseLR.tfkeras_Adam_lr_decay(
-        weight_name_to_layer_depth=weight_name_to_layer_depth,
-        n_layers=n_layers,
-        layerwise_lr_decay_power=0.8,  # 学习率逐层衰减的权重
-        lr=1e-3  # 基础学习率，即模型最后一层的学习率，其他层的学习率在此基础上衰减
-    )
-    model.compile(
-        optimizer=optimizer,
-        loss=keras.losses.binary_crossentropy
-    )
-    print('model compile done!')
-    x_train = np.random.random(size=[100,5])  # data size: 100
-    y_train = np.array([1, 0]*50)
-    model.fit(x_train, y_train, epochs=3, verbose=2)
-
-    # test success!
-    print('tf version: ', tf.__version__, ', keras version: ', keras.__version__)
-    print('test_tfkeras_Adam_lr_decay ok!')
-
-
 if __name__=='__main__':
     # test_keras_Adam_2lr()
-    # test_tfkeras_Adam_2lr()
     # test_keras_Adam_3lr()
-    # test_tfkeras_Adam_3lr()
     test_keras_Adam_lr_decay()
-    test_tfkeras_Adam_lr_decay()
+
