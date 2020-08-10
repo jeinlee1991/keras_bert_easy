86102a5d855f3664cb52d5ab99681aeb2639dd68
washington <947268074@qq.com>
2020-08-10 16:00:21 +0800

    增加weight_decay配置，可将Adam变成AdamW

--- keras_bert_easy/optimizers/layerwiseLR.py
+++ keras_bert_easy/optimizers/layerwiseLR.py
@@ -3,6 +3,7 @@
 # 使用分层学习率的优化器，往往能得到更好的效果。
 
 import os
+import re
 from distutils.util import strtobool
 import tensorflow as tf
 
@@ -42,6 +43,15 @@ def keras_mode_hint():
         print('using keras API. keras version: %s' % version)
 
 
+def do_use_weight_decay(param_name, exclude_from_weight_decay):
+    """Whether to use L2 weight decay for `param_name`."""
+    if exclude_from_weight_decay:
+        for r in exclude_from_weight_decay:
+            if re.search(r, param_name) is not None:
+                return False
+    return True
+
+
 class keras_Adam_2lr(BaseOptimizer):
     """支持2段式学习率的Adam optimizer.
     # Arguments
@@ -50,32 +60,35 @@ class keras_Adam_2lr(BaseOptimizer):
         beta_1: float, 0 < beta < 1. Generally close to 1.
         beta_2: float, 0 < beta < 1. Generally close to 1.
         epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
-        decay: float >= 0. Learning rate decay over each update.
         amsgrad: boolean. Whether to apply the AMSGrad variant of this
             algorithm from the paper "On the Convergence of Adam and
             Beyond".
     """
 
     def __init__(self, final_layers, lr=(1e-5, 1e-4),
-                 beta_1=0.9, beta_2=0.999,
-                 epsilon=None, decay=0., amsgrad=False, **kwargs):
+                 beta_1=0.9, beta_2=0.999, epsilon=None,
+                 weight_decay=0, # 0.01
+                 exclude_from_weight_decay=("LayerNorm", "layer_norm", "bias"),
+                 amsgrad=False, **kwargs):
         super(keras_Adam_2lr, self).__init__(**kwargs)
 
         keras_mode_hint()
+        if weight_decay > 0:
+            print('using optimizer AdamW...')
 
         with K.name_scope(self.__class__.__name__):
             self.iterations = K.variable(0, dtype='int64', name='iterations')
             self.lrate = K.variable(lr, name='lr')
             self.beta_1 = K.variable(beta_1, name='beta_1')
             self.beta_2 = K.variable(beta_2, name='beta_2')
-            self.decay = K.variable(decay, name='decay')
 
             self.final_layers = final_layers
 
         if epsilon is None:
             epsilon = K.epsilon()
         self.epsilon = epsilon
-        self.initial_decay = decay
+        self.weight_decay = weight_decay
+        self.exclude_from_weight_decay = exclude_from_weight_decay
         self.amsgrad = amsgrad
 
     def get_updates(self, loss, params):
@@ -83,10 +96,6 @@ class keras_Adam_2lr(BaseOptimizer):
         self.updates = [K.update_add(self.iterations, 1)]
 
         lr = self.lrate
-        if self.initial_decay > 0:
-            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations,
-                                                      K.dtype(self.decay))))
-
         t = K.cast(self.iterations, K.floatx()) + 1
         lr_t = lr * (K.sqrt(1. - K.pow(self.beta_2, t)) /
                      (1. - K.pow(self.beta_1, t)))
@@ -116,6 +125,18 @@ class keras_Adam_2lr(BaseOptimizer):
             else:
                 p_t = p - lr_grp * m_t / (K.sqrt(v_t) + self.epsilon)
 
+            # weight decay (L2 regularization)
+            # Just adding the square of the weights to the loss function is *not*
+            # the correct way of using L2 regularization/weight decay with Adam,
+            # since that will interact with the m and v parameters in strange ways.
+            #
+            # Instead we want ot decay the weights in a manner that doesn't interact
+            # with the m/v parameters. This is equivalent to adding the square
+            # of the weights to the loss with plain (non-momentum) SGD.
+            if self.weight_decay > 0:
+                if do_use_weight_decay(p.name, self.exclude_from_weight_decay):
+                    p_t = p_t - lr_grp * self.weight_decay * p
+
             self.updates.append(K.update(m, m_t))
             self.updates.append(K.update(v, v_t))
             new_p = p_t
@@ -132,8 +153,8 @@ class keras_Adam_2lr(BaseOptimizer):
             'lr': (K.get_value(self.lrate)),
             'beta_1': float(K.get_value(self.beta_1)),
             'beta_2': float(K.get_value(self.beta_2)),
-            'decay': float(K.get_value(self.decay)),
             'epsilon': self.epsilon,
+            'weight_decay': self.weight_decay,
             'amsgrad': self.amsgrad}
         base_config = super(keras_Adam_2lr, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
